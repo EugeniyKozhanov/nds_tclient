066e2d5224632119c3f9301ccd304b4625756354
Docker image user <eugeniy.kozhanov@gmail.com>
2023-01-09 13:29:49 +0000

    fix test code format

--- test/sslexample_test.cc
+++ test/sslexample_test.cc
@@ -2,29 +2,28 @@
 
 #include <gtest/gtest.h>
 
-
 TEST(SslExample, EncryptAndDecrypt) {
     std::string str("Test Enc/Dec string!");
-	nds::SslExample a;
-    
-	auto [code_encr, encr] = a.encrypt(str);
-    
-	EXPECT_EQ(static_cast<int>(code_encr.value()), static_cast<int>(nds::detail::SslError::Success));
-    
-	auto [code_decr, decr] = a.decrypt(encr);
+    nds::SslExample a;
 
-	EXPECT_EQ(static_cast<int>(code_decr.value()), static_cast<int>(nds::detail::SslError::Success));
-	EXPECT_STREQ(str.c_str(), decr.c_str());
+    auto [code_encr, encr] = a.encrypt(str);
+
+    EXPECT_EQ(static_cast<int>(code_encr.value()), static_cast<int>(nds::detail::SslError::Success));
+
+    auto [code_decr, decr] = a.decrypt(encr);
+
+    EXPECT_EQ(static_cast<int>(code_decr.value()), static_cast<int>(nds::detail::SslError::Success));
+    EXPECT_STREQ(str.c_str(), decr.c_str());
 }
 
 TEST(SslExample, ErrorCodesOffset) {
-	int encr_offset = static_cast<int>(nds::detail::SslError::EncInit);
+    int encr_offset = static_cast<int>(nds::detail::SslError::EncInit);
 
-	EXPECT_EQ(encr_offset+1, static_cast<int>(nds::detail::SslError::EncUpdate));
-	EXPECT_EQ(encr_offset+2, static_cast<int>(nds::detail::SslError::EncFinal));
+    EXPECT_EQ(encr_offset + 1, static_cast<int>(nds::detail::SslError::EncUpdate));
+    EXPECT_EQ(encr_offset + 2, static_cast<int>(nds::detail::SslError::EncFinal));
 
-	int decr_offset = static_cast<int>(nds::detail::SslError::DecInit);
+    int decr_offset = static_cast<int>(nds::detail::SslError::DecInit);
 
-	EXPECT_EQ(decr_offset+1, static_cast<int>(nds::detail::SslError::DecUpdate));
-	EXPECT_EQ(decr_offset+2, static_cast<int>(nds::detail::SslError::DecFinal));
+    EXPECT_EQ(decr_offset + 1, static_cast<int>(nds::detail::SslError::DecUpdate));
+    EXPECT_EQ(decr_offset + 2, static_cast<int>(nds::detail::SslError::DecFinal));
 }
