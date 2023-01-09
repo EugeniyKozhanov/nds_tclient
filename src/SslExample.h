5b33c0608aab39b2e093dda56cc4d022f8d59400
Docker image user <eugeniy.kozhanov@gmail.com>
2023-01-09 12:08:19 +0000

    apply google code style format

--- src/SslExample.h
+++ src/SslExample.h
@@ -1,166 +1,158 @@
 #pragma once
 
-#include <string>
-#include <utility>
-#include <tuple>
-#include <system_error>
-
 #include <openssl/conf.h>
-#include <openssl/evp.h>
 #include <openssl/err.h>
+#include <openssl/evp.h>
 
+#include <string>
+#include <system_error>
+#include <tuple>
+#include <utility>
 
 namespace nds {
 
-namespace detail
-{
+namespace detail {
 
-	enum class SslError
-	{
-		Success = 0,
-		ContextCreate,
-		EncInit,
-		EncUpdate,
-		EncFinal,
-		DecInit,
-		DecUpdate,
-		DecFinal
-	};
+enum class SslError {
+    Success = 0,
+    ContextCreate,
+    EncInit,
+    EncUpdate,
+    EncFinal,
+    DecInit,
+    DecUpdate,
+    DecFinal
+};
 
-	enum class SslErrorConditional
-	{
-		ERROR_CODE = 0,
-		UNKNOWN_ERROR
-	};
+enum class SslErrorConditional {
+    ERROR_CODE = 0,
+    UNKNOWN_ERROR
+};
 
-	class SslErrorCategory : public std::error_category
-	{
-		public :
+class SslErrorCategory : public std::error_category {
+   public:
+    SslErrorCategory() noexcept {};
+    virtual const char* name() const noexcept override;
+    virtual std::string message(int ev) const override;
 
-			SslErrorCategory() noexcept {};
-			virtual const char* name() const noexcept override;
-			virtual std::string message(int ev) const override;
+    std::error_condition default_error_condition(int err_value) const noexcept override;
+    bool equivalent(const std::error_code& err_code, int err_value) const noexcept override;
+    bool equivalent(int err_value, const std::error_condition& err_cond) const noexcept override;
 
-			std::error_condition default_error_condition(int err_value) const noexcept override;
-			bool equivalent(const std::error_code& err_code, int err_value) const noexcept override;
-			bool equivalent(int err_value, const std::error_condition& err_cond) const noexcept override;
+   private:
+    SslErrorCategory(const SslErrorCategory&) = delete;
+    SslErrorCategory(SslErrorCategory&&) = delete;
+    SslErrorCategory& operator=(const SslErrorCategory&) = delete;
+    SslErrorCategory& operator=(SslErrorCategory&&) = delete;
 
-		private :
+} const SslErrorCategory;
 
-			SslErrorCategory(const SslErrorCategory&) = delete;
-			SslErrorCategory(SslErrorCategory&&) = delete;
-			SslErrorCategory& operator=(const SslErrorCategory&) = delete;
-			SslErrorCategory& operator=(SslErrorCategory&&) = delete;
+inline std::error_code make_error_code(nds::detail::SslError error) {
+    return std::error_code(static_cast<int>(error), nds::detail::SslErrorCategory);
+}
 
-	}const SslErrorCategory;
+inline std::error_condition make_error_condition(nds::detail::SslErrorConditional condition) noexcept {
+    return std::error_condition(static_cast<int>(condition), nds::detail::SslErrorCategory);
+}
 
-	inline std::error_code make_error_code(nds::detail::SslError error)
-	{
-		return std::error_code(static_cast<int>(error), nds::detail::SslErrorCategory);
-	}
-
-	inline std::error_condition make_error_condition(nds::detail::SslErrorConditional condition) noexcept
-	{
-		return std::error_condition(static_cast<int>(condition), nds::detail::SslErrorCategory);
-	}
-
-} // namespace detail
+}  // namespace detail
 
 struct SslAesConfig {
-	const EVP_CIPHER* type;
-	ENGINE* impl;
-	unsigned char* key;
-	unsigned char* iv;
+    const EVP_CIPHER* type;
+    ENGINE* impl;
+    unsigned char* key;
+    unsigned char* iv;
 };
 extern struct SslAesConfig aesSimpleConfig;
 
-class SslExample
-{
-	public :
+class SslExample {
+   public:
+    SslExample();
 
-		SslExample();
+    SslExample(const SslExample&) = delete;
+    SslExample& operator=(const SslExample&) = delete;
 
-		SslExample(const SslExample&) = delete;
-		SslExample & operator= (const SslExample&) = delete;
+    SslExample(SslExample&& other);
+    SslExample& operator=(SslExample&& other);
 
-		SslExample(SslExample&& other);		
-		SslExample& operator=(SslExample&& other);
+    ~SslExample();
 
-		~SslExample();
+    std::pair<std::error_code, std::string> decrypt(const std::string& text) {
+        std::string decr;
+        std::error_code error = process<decltype(EVP_DecryptInit_ex), decltype(EVP_DecryptUpdate), decltype(EVP_DecryptFinal_ex)>(
+            EVP_DecryptInit_ex,
+            EVP_DecryptUpdate,
+            EVP_DecryptFinal_ex,
+            text,
+            decr,
+            static_cast<int>(detail::SslError::DecInit));
 
-		std::pair<std::error_code, std::string> decrypt(const std::string& text)
-		{
-			std::string decr;
-			std::error_code error = process<decltype(EVP_DecryptInit_ex), decltype(EVP_DecryptUpdate), decltype(EVP_DecryptFinal_ex)>
-													(EVP_DecryptInit_ex, EVP_DecryptUpdate, EVP_DecryptFinal_ex, 
-													 text, decr, static_cast<int>(detail::SslError::DecInit));
-			return std::make_pair(error, decr);
-		}
+        return std::make_pair(error, decr);
+    }
 
-		std::pair<std::error_code, std::string> encrypt(const std::string& text)
-		{
-			std::string encr;
-			std::error_code error = process<decltype(EVP_EncryptInit_ex), decltype(EVP_EncryptUpdate), decltype(EVP_EncryptFinal_ex)>
-													(EVP_EncryptInit_ex, EVP_EncryptUpdate, EVP_EncryptFinal_ex, 
-													 text, encr, static_cast<int>(detail::SslError::EncInit));
-			return std::make_pair(error, encr);
-		}
+    std::pair<std::error_code, std::string> encrypt(const std::string& text) {
+        std::string encr;
+        std::error_code error = process<decltype(EVP_EncryptInit_ex), decltype(EVP_EncryptUpdate), decltype(EVP_EncryptFinal_ex)>(
+            EVP_EncryptInit_ex,
+            EVP_EncryptUpdate,
+            EVP_EncryptFinal_ex,
+            text,
+            encr,
+            static_cast<int>(detail::SslError::EncInit));
 
-	private :
+        return std::make_pair(error, encr);
+    }
 
-		EVP_CIPHER_CTX* _ctx;
+   private:
+    EVP_CIPHER_CTX* _ctx;
 
-		template<class F1, class F2, class F3>
-		std::error_code process(F1 init_f, F2 update_f, F3 final_f, const std::string& orig_text, std::string& new_text, int error_offset)
-		{
-			std::error_code ret = detail::SslError::Success;
+    template <class F1, class F2, class F3>
+    std::error_code process(F1 init_f, F2 update_f, F3 final_f, const std::string& orig_text, std::string& new_text, int error_offset) {
+        std::error_code ret = detail::SslError::Success;
 
-			if (!(_ctx = EVP_CIPHER_CTX_new()))
-			{
-				ret = detail::SslError::ContextCreate;
-				return ret;
-			} 
+        if (!(_ctx = EVP_CIPHER_CTX_new())) {
+            ret = detail::SslError::ContextCreate;
+            return ret;
+        }
 
-			auto [chipher, eingeine, key, iv] = aesSimpleConfig;
-			if (1 != std::apply(init_f, std::make_tuple(_ctx, chipher, eingeine, key, iv)))
-			{
-				ret = static_cast<detail::SslError>(error_offset);
-				return ret;
-			}
+        auto [chipher, eingeine, key, iv] = aesSimpleConfig;
+        if (1 != std::apply(init_f, std::make_tuple(_ctx, chipher, eingeine, key, iv))) {
+            ret = static_cast<detail::SslError>(error_offset);
+            return ret;
+        }
 
-			// TODO:
-  			unsigned char buffer[128];
-			int buffer_len = 0;
-			int current_len;
+        // TODO:
+        unsigned char buffer[128];
+        int buffer_len = 0;
+        int current_len;
 
-			if (1 != std::apply(update_f, std::make_tuple(_ctx, buffer, &current_len, (const unsigned char*)(orig_text.c_str()), orig_text.length())))
-			{
-				ret = static_cast<detail::SslError>(error_offset+1);
-				return ret;
-			}
-			buffer_len = current_len;
+        if (1 != std::apply(update_f, std::make_tuple(_ctx, buffer, &current_len, (const unsigned char*)(orig_text.c_str()), orig_text.length()))) {
+            ret = static_cast<detail::SslError>(error_offset + 1);
+            return ret;
+        }
+        buffer_len = current_len;
 
-			if (1 != std::apply(final_f, std::make_tuple(_ctx, buffer+current_len, &current_len)))
-			{
-				ret = static_cast<detail::SslError>(error_offset+2);
-				return ret;
-			}
-			buffer_len +=current_len;
+        if (1 != std::apply(final_f, std::make_tuple(_ctx, buffer + current_len, &current_len))) {
+            ret = static_cast<detail::SslError>(error_offset + 2);
+            return ret;
+        }
+        buffer_len += current_len;
 
-			new_text = std::string((char*)buffer, buffer_len);
+        new_text = std::string((char*)buffer, buffer_len);
 
-			void cleanup(); 
+        void cleanup();
 
-			return ret;
-		}
+        return ret;
+    }
 
-		void cleanup(); 
+    void cleanup();
 };
 
-} // namespace nds
- 
-namespace std
-{
-    template <> struct is_error_condition_enum<nds::detail::SslErrorConditional> : public true_type {};
-    template <> struct is_error_code_enum<nds::detail::SslError> : public true_type {};
-} // namespace std
+}  // namespace nds
+
+namespace std {
+template <>
+struct is_error_condition_enum<nds::detail::SslErrorConditional> : public true_type {};
+template <>
+struct is_error_code_enum<nds::detail::SslError> : public true_type {};
+}  // namespace std
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,44 @@
-#include "SslExample.h"
-
 #include <nds.h>
-
 #include <stdio.h>
 #include <string.h>
 
+#include "SslExample.h"
+
 volatile int frame = 0;
 
-void Vblank()
-{
-	frame++;
+void Vblank() {
+    frame++;
 }
-	
-int main(void)
-{
-	nds::SslExample a;
-	std::string str("Test Enc/Dec string!");
 
-	irqSet(IRQ_VBLANK, Vblank);
-	consoleDemoInit();
+int main(void) {
+    nds::SslExample a;
+    std::string str("Test Enc/Dec string!");
 
-	iprintf(" [*] Start test OpenSSL build:\n");
+    irqSet(IRQ_VBLANK, Vblank);
+    consoleDemoInit();
 
-	auto [code_enc, enc] = a.encrypt(str);
-	iprintf(" [*] - enc result: %s\n", code_enc.message().c_str());
+    iprintf(" [*] Start test OpenSSL build:\n");
 
-	if (!code_enc.value())
-	{
-		auto [code_dec, dec] = a.decrypt(enc);
-		iprintf(" [*] - dec result: %s\n", code_dec.message().c_str());
-	
-		if (code_dec.value())
-		{
-			iprintf(" [*] - dec result code: %s\n", code_dec.value());
-		}
-		else
-		{
-			iprintf(" [*] - dec result text: %s\n", dec.c_str());
-			iprintf(" [*] - enc result text: %s\n", enc.c_str());
-		}
-	}
-	else
-	{
-		iprintf(" [*] - enc result code: %s\n", code_enc.value());
-	}
+    auto [code_enc, enc] = a.encrypt(str);
+    iprintf(" [*] - enc result: %s\n", code_enc.message().c_str());
 
-	while(1)
-	{
-		swiWaitForVBlank();
-	}
+    if (!code_enc.value()) {
+        auto [code_dec, dec] = a.decrypt(enc);
+        iprintf(" [*] - dec result: %s\n", code_dec.message().c_str());
 
-	return 0;
+        if (code_dec.value()) {
+            iprintf(" [*] - dec result code: %s\n", code_dec.value());
+        } else {
+            iprintf(" [*] - dec result text: %s\n", dec.c_str());
+            iprintf(" [*] - enc result text: %s\n", enc.c_str());
+        }
+    } else {
+        iprintf(" [*] - enc result code: %s\n", code_enc.value());
+    }
+
+    while (1) {
+        swiWaitForVBlank();
+    }
+
+    return 0;
 }
